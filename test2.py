8e01ce58428f87ffa7ed1cd9bc629b563e517ced
dinomonster <pengzheno_1@163.com>
2017-06-17 15:50:35 +0800

    update

--- test2.py
+++ test2.py
@@ -14,20 +14,22 @@ print("z:"+z)
 
 
 #String字符串
-str = "Python"
-# str[0] = "1" 字符串不可被改变
-print(str)
-print(str[0])
-print(str[0:-1])
-print(str[1:4])
-print(str[-3:-1])
-print(str[2:])
-print(str*2)
-print("hello "+str)
-print("\n"+str)
-print(r"\n"+str)#前面加r 使反斜杠不发生转义
+print("============字符串操作===============")
+s = "Python"
+# s[0] = "1" 字符串不可被改变
+print(s)
+print(s[0])
+print(s[0:-1])
+print(s[1:4])
+print(s[-3:-1])
+print(s[2:])
+print(s*2)
+print("hello "+s)
+print("\n"+s)
+print(r"\n"+s)#前面加r 使反斜杠不发生转义
 
 #List 列表
+print("============列表操作===============")
 list1 = [123,"abc",333,"bbbb"]
 list2 = [555,"lplpk"]
 print(list1)
@@ -41,4 +43,52 @@ list1[1] = "-----"
 print(list1)
 
 #Tuple 元组
+print("============元组操作===============")
 tuple1 = ("asdas",1234,"asfasfa","vcxb",252)
+tuple2 = ("cccc",888)
+tuple3 = (list1,list2)
+#tuple1[0] = "===" 元组不可变
+print(tuple1)
+print(tuple1[0])
+print(tuple1[0:-1])
+print(tuple1[1:3])
+print(tuple1[1:])
+print(tuple1*2)
+print(tuple1+tuple2)
+tuple3[0][0] = "111"#元组不可变，但是能包含可变的对象，比如list列表
+print(tuple3)
+
+#Set 集合 是一个无序不重复元素的序列。
+print("============集合操作===============")
+student = {"tom","jerry","wangwang","tom","Tom"}
+print(student)
+if('tom' in student):
+	print("tom在集合中")
+else:
+	print("tom不在集合中")
+a = set('tomasdasdf')
+b = set('cvzafgsdfa')
+
+print(a)
+print(a-b)# a和b的差集
+print(a|b)# a和b的并集
+print(a&b)# a和b的交集
+print(a^b)# a和b中不同时存在的元素
+
+'''Dictionary 字典 列表是有序的对象结合，字典是无序的对象集合。
+两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取
+'''
+print("============字典操作===============")
+dict = {} #注意{}创建一个空字典，创建空集合必须用set()
+dict['aaa'] = "hello"
+dict[2] = "world"
+dict2 = {'name':'pengzhe','age':26}
+print(dict)
+print(dict['aaa'])
+print(dict[2])
+print(dict2.keys())
+print(dict2.values())
+
+dict2['age'] = str(dict2['age'])#类型转换
+print(dict2.values())
+
